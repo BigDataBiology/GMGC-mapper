0a91125c6990a8ae0893a84e339e48e299cad1f7
Luis Pedro Coelho <luis@luispedro.org>
2020-05-20 18:00:49 +0200

    TST Test that splits include originals
    At least, check that the number of sequences matches

--- test/test_split.py
+++ test/test_split.py
@@ -18,3 +18,32 @@ def test_split(tmpdir):
     assert split_file(faa_file, fna_file, odir, 2) == 3
     assert split_file(faa_file, fna_file, odir, 3) == 2
     assert split_file(faa_file, fna_file, odir, 4) == 2
+
+
+def test_split(tmpdir):
+    faa_file = path.join(tmpdir, 'input.faa')
+    fna_file = path.join(tmpdir, 'input.faa')
+
+    total = 20
+
+    # Both faa_file & fna_file will have `total` elements:
+    with open(faa_file, 'wt') as out_a:
+        with open(fna_file, 'wt') as out_n:
+            for i in range(total):
+                out_a.write(f'>g{i}\nMEPTAP\n')
+                out_n.write(f'>g{i}\nATGTTC\n')
+
+        
+    odir = path.join(tmpdir, 'output')
+    n = split_file(faa_file, fna_file, odir, 11)
+    total_nt = 0
+    total_aa =0
+    for it in range(n):
+        with open(path.join(odir, 'split_{}.faa'.format(it+1))) as ifile:
+            for line in ifile:
+                total_aa += (line[0] == '>')
+        with open(path.join(odir, 'split_{}.fna'.format(it+1))) as ifile:
+            for line in ifile:
+                total_nt += (line[0] == '>')
+    assert total_nt == total
+    assert total_aa == total
