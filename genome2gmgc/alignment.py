01648a3b3805552ccaee84a301495d73af5868ef
psj1997 <1176894699@qq.com>
2020-05-22 22:26:56 +0800

    fix bug of calculating coverage

--- genome2gmgc/alignment.py
+++ genome2gmgc/alignment.py
@@ -1,6 +1,6 @@
 import skbio.alignment
 from skbio.sequence import DNA,Protein
-from .BLOSUM import blosum62,blosum50
+from BLOSUM import blosum62,blosum50
 
 def num_alignment(query,target):
     num = 0
@@ -31,19 +31,18 @@ def identity_coverage(dna_query,protein_query,dna_target,protein_target):
     except:
         sw_dna = skbio.alignment.local_pairwise_align_nucleotide(DNA(dna_query), DNA(dna_target))
     dna_identity,align_length = extract_sw(sw_dna)
-    dna_coverage = align_length / len(dna_target)
-
+    dna_coverage = align_length / min(len(dna_query),len(dna_target))
     if dna_identity >= 0.95 and dna_coverage >= 0.95:
 
         return 'EXACT'
 
     else:
         try:
-            sw_protein = skbio.alignment.local_pairwise_align_ssw(Protein(protein_query),Protein(protein_target),substitution_matrix = blosum62 )
+            sw_protein = skbio.alignment.local_pairwise_align_ssw(Protein(protein_query),Protein(protein_target),substitution_matrix = blosum62,gap_open_penalty=11,gap_extend_penalty=1)
         except:
-            sw_protein = skbio.alignment.local_pairwise_align_protein(Protein(protein_query),Protein(protein_target),substitution_matrix = blosum62 )
+            sw_protein = skbio.alignment.local_pairwise_align_protein(Protein(protein_query),Protein(protein_target),substitution_matrix = blosum62,gap_open_penalty=11,gap_extend_penalty=1)
         protein_identity,align_length = extract_sw(sw_protein)
-        protein_coverage = align_length / len(protein_target)
+        protein_coverage = align_length / min(len(protein_query),len(protein_target))
         if protein_identity >= 0.8 and protein_coverage >= 0.8:
             return 'SIMILAR'
 
@@ -51,5 +50,3 @@ def identity_coverage(dna_query,protein_query,dna_target,protein_target):
             return 'MATCH'
 
         return 'NO MATCH'
-
-
