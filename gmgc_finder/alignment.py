9024c4bbb5d8a2255d4a5ecec5f735e82726db9f
psj1997 <1176894699@qq.com>
2020-06-12 20:34:38 +0800

    fix bug of testing

--- gmgc_finder/alignment.py
+++ gmgc_finder/alignment.py
@@ -37,6 +37,26 @@ def identity_coverage(dna_query,protein_query,dna_target,protein_target):
 
             return 'EXACT'
 
+        else:
+            try:
+                sw_protein = skbio.alignment.local_pairwise_align_ssw(Protein(protein_query), Protein(protein_target),
+                                                                      substitution_matrix=blosum62, gap_open_penalty=11,
+                                                                      gap_extend_penalty=1)
+            except:
+                sw_protein = skbio.alignment.local_pairwise_align_protein(Protein(protein_query),
+                                                                          Protein(protein_target),
+                                                                          substitution_matrix=blosum62,
+                                                                          gap_open_penalty=11, gap_extend_penalty=1)
+            protein_identity, align_length = extract_sw(sw_protein)
+            protein_coverage = align_length / min(len(protein_query), len(protein_target))
+            if protein_identity >= 0.8 and protein_coverage >= 0.8:
+                return 'SIMILAR'
+
+            if protein_identity >= 0.5 and protein_coverage >= 0.5:
+                return 'MATCH'
+
+            return 'NO MATCH'
+
     else:
         try:
             sw_protein = skbio.alignment.local_pairwise_align_ssw(Protein(protein_query),Protein(protein_target),substitution_matrix = blosum62,gap_open_penalty=11,gap_extend_penalty=1)
